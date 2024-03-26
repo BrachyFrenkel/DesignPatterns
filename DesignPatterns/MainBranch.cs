fe1aa958f50a3ae4f6d75a2bd0e94a79329308b2
BrachyFrenkel <brachy474@gmail.com>
2024-03-26 15:19:13 +0200

    start project

---
+++ DesignPatterns/MainBranch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns;
+public class MainBranch : Branch 
+{ 
+    private MainBranch MainBranchInstance = new MainBranch();
+    private MainBranch()
+    {
+        this.MainBranchInstance= MainBranchInstance;
+    }
+    public MainBranch GetMainBranch()
+    {
+        return this.MainBranchInstance; 
+    }
+
+}
---
+++ DesignPatterns/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns;
+public class User
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Acces { get; set; }
+
+    //public List<BranchItem> Items { get; set; }
+    //public List<Command> Commands { get; set; }
+}
